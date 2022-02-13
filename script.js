d01d95539151c4c9046a61e141848927bbeb87ba
Dávid Jopčík <david.jopcik@gmail.com>
2022-02-13 11:55:40 +0100

    Cycles

--- script.js
+++ script.js
@@ -1,22 +1,67 @@
-let pole = [1, 2, 3, 4, 5, 6]
+/* 
+const pole = [1, 2, 3, 4, 5, 6];
+const novePole = [];
+pole.forEach((x) => {
+  if (x % 2 === 0) {
+    novePole.push(x);
+  }
+});
+console.log(novePole);
+function filtrovanie(pole, filtrovanie) {
+const novePole = [];
+  pole.forEach((x) => {
+    if (filtrovanie(x)) {
+      novePole.push(x);
+    }
+  });
+  console.log(novePole);
+  return novePole
+}
+console.log(filtrovanie(pole, x => x > 3 && x < 5))
+ */
 
-console.log(pole.filter(x => {
-    return x%2 === 0
-}));
 
-let novePole = []
-for (let i = 0; i < pole.length; i++) {
-    if(pole[i]%2 ===0 ){
-        novePole.push(pole[i])
+
+//cyklus while - vypis do pola od 10 do 20
+let pole = []
+let cislo = 10
+
+while (cislo <= 20) {
+    cislo % 2 === 0
+        pole.push(cislo)
+        cislo++;
+    }
+console.log(pole);
+console.log("--------");
+
+
+//cyklus while + parne
+let pole2 = []
+let cislo2 = 10
+
+    while (cislo2 <= 20) {
+        if(cislo2 % 2 === 0){
+            pole2.push(cislo2)
+            }
+            cislo2++;
+        }
+console.log(pole2);
+console.log("--------");
+
+//cyklus for
+let pole3 = []
+let cislo3 = 10
+for (let i = 0; i < 20; i++) {
+    pole3.push(cislo3)
+    cislo3 +=1
+    if (cislo3 === 20) {
+        break
     }
 }
+console.log(pole3);
+console.log("--------");
 
-pole.forEach(x => {
-   if(x%2 ===0) {
-    novePole.push(x)
-   } 
-})
 
-console.log(novePole);
+
 
 
